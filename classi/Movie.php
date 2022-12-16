0f0e893ad5dd633f074bd18f76620344298e1f11
alessandrolega <alessandrolega24@gmail.com>
2022-12-16 16:36:55 +0100

    definito costruttore dentro la classe

--- classi/Movie.php
+++ classi/Movie.php
@@ -3,5 +3,15 @@
         // ATTRIBUTI CLASSE
         public $title;
         public $genre;
+        public $age;
+
+        // COSTRUTTORE
+
+        function __construct( $_x, $_y, $_z){
+            $this->title = $_x;
+            $this->genre = $_y;
+            $this->age = $_z;
+
+        }
     }
 ?>
